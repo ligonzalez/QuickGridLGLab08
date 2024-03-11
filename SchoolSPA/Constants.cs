ebf7508e4ef5517350ff9beb6a2aae189d8443d3
“ligonzalez” <lauragonzalez@yahooe.com>
2024-03-11 16:48:52 -0700

    QuickGrid first check in

---
+++ SchoolSPA/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolSPA;
+
+public class Constants
+{
+    //public const string SchoolAPIBase = "http://localhost:5208/api/";
+    public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
+    //public const string SchoolAPIBase = "http://localhost:5111/api/"
+    
+}
