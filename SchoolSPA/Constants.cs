1c0f9369f940b89221888c8366718236958caf19
“ligonzalez” <lauragonzalez@yahooe.com>
2024-03-12 13:37:36 -0700

    fix api url

--- SchoolSPA/Constants.cs
+++ SchoolSPA/Constants.cs
@@ -3,7 +3,7 @@
 public class Constants
 {
     //public const string SchoolAPIBase = "http://localhost:5208/api/";
-    //public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
+    public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
     //public const string SchoolAPIBase = "http://localhost:5111/api/"
-    public const string SchoolAPIBase = "/api/";
+    //public const string SchoolAPIBase = "/api/";
 }
